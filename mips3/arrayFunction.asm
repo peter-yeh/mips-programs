6151972f4b1fda6ede465aa28b2d6ef1375067a2
peter-yeh <yehyuchun2@gmail.com>
2020-09-23 16:25:33 +0800

    Update formatting

--- mips3/arrayFunction.asm
+++ mips3/arrayFunction.asm
@@ -73,22 +73,22 @@ main:
 #Assumption: Array element is word size (4-byte)
 
 printArray:
-	addi $t1, $a0, 0	# $t1 is the pointer to the item
-	sll  $t2, $a1, 2	# $t2 is the offset beyond the last item
-	add  $t2, $a0, $t2 	# $t2 is pointing beyond the last item
+        addi    $t1, $a0, 0	          # $t1 is the pointer to the item
+        sll     $t2, $a1, 2	          # $t2 is the offset beyond the last item
+        add     $t2, $a0, $t2 	      # $t2 is pointing beyond the last item
 l1:	
-	beq  $t1, $t2, e1
-	lw   $t3, 0($t1)	# $t3 is the current item
-	li   $v0, 1   		# system call code for print_int
-	addi $a0, $t3, 0    # integer to print
-	syscall       		# print it
-	addi $t1, $t1, 4
-	j l1				# Another iteration
+        beq     $t1, $t2, e1
+        lw      $t3, 0($t1)	          # $t3 is the current item
+        li      $v0, 1   		      # system call code for print_int
+        addi    $a0, $t3, 0           # integer to print
+        syscall       		          # print it
+        addi    $t1, $t1, 4
+        j       l1				      # Another iteratio n
 e1:
-	li   $v0, 4   		# system call code for print_string
-	la   $a0, newl    	# 
-	syscall       		# print newline
-	jr 	$ra				# return from this function
+        li      $v0, 4,               # system call code for print_string
+        la      $a0, newl
+        syscall       		          # print newline
+        jr 	    $ra				      # return from this function
 
 
 ##################################################################
